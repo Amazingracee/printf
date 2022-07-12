97eb8df0d494ff8788e66f67970a0df1461b01d0
Totweva <totweva@hmail.com>
2022-07-12 07:04:36 -0700

    my commit

---
+++ print_short_unst.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * prinhunst - prints a short unsigned integer
+ * @arguments: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+
+int prinhunst(va_list data, char *buf, unsigned int ibuf)
+{
+	unsigned short int int_in, int_temp, i, div;
+	int_in = va_arg(data, unsigned int);
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = mang_buffer(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
+}
